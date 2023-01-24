ef6e9ee80d6849143422806b4e4070d03bf0ffa4
newgr <newgrigory@yandex.ru>
2023-01-24 15:22:01 +0400

    add distance

--- classes.py
+++ classes.py
@@ -5,3 +5,8 @@ class Point:
 
     def __str__(self):
         return f'({self.x}, {self.y})'
+
+    def get_distance(self, point):
+        vector_x = (point.x - self.x) ** 2
+        vector_y = (point.y - self.y) ** 2
+        return (vector_x + vector_y) ** 0.5
