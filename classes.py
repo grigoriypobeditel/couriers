3a8a587af4824c94facaa0df242f6243f74ad655
newgr <newgrigory@yandex.ru>
2023-01-24 15:27:53 +0400

    add order, couriers

--- classes.py
+++ classes.py
@@ -10,3 +10,26 @@ class Point:
         vector_x = (point.x - self.x) ** 2
         vector_y = (point.y - self.y) ** 2
         return (vector_x + vector_y) ** 0.5
+
+
+class Courier(Point):
+    def __init__(self, x, y, name, weight, speed):
+        super().__init__(x, y)
+        self.name = name
+        self.weight = weight
+        self.speed = speed
+        self.orders = []
+
+
+class Order(Point):
+    def __init__(self, x, y, name, weight):
+        super().__init__(x, y)
+        self.name = name
+        self.weight = weight
+        self.time = {
+            "get_order": 0,
+            "out_order": 0
+        }
+
+    def __repr__(self):
+        return self.name
